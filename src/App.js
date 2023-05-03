98fc948a30a6ede531d4f806518447077ffd7282
metelitt <104203274+metelitt@users.noreply.github.com>
2023-05-03 21:06:44 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import Container from "@mui/material/Container";
 import { Header } from "./components";
 import { Home, FullPost, Registration, AddPost, Login } from "./pages";
 import { Route, Routes } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchAuthMe,selectIsAuth } from "./redux/slices/auth";
 
 function App() {
+  const isAuth = useSelector(selectIsAuth);
+  const dispatch=useDispatch()
+  useEffect(()=>{
+    dispatch(fetchAuthMe())
+  },[])
   return (
     <>
       <Header />
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,12 +4,18 @@ import Button from '@mui/material/Button';
 import styles from './Header.module.scss';
 import Container from '@mui/material/Container';
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, selectIsAuth } from '../../redux/slices/auth';
 
 export const Header = () => {
-  const isAuth = false;
-
-  const onClickLogout = () => {};
-
+  const isAuth = useSelector(selectIsAuth);
+const dispatch=useDispatch()
+  const onClickLogout = () => {;
+    if(window.confirm('Ты точно хочешь выйти?')){
+      dispatch(logout())
+      window.localStorage.removeItem('token')
+    }
+  }
   return (
     <div className={styles.root}>
       <Container maxWidth="lg">
